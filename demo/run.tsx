ec6ce6759f4c0d5a1df3ef0bc32a5f386664dc98
CI <CI@github>
2020-04-14 01:45:30 +0900

    Add demo

---
+++ demo/run.tsx
@@ -0,0 +1,32 @@
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+
+const Editor = React.lazy(() => import("../lib/index"));
+
+function App() {
+  return (
+    <>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `html, body, main { margin: 0; padding: 0; width: 100vw; height: 100vh; }`,
+        }}
+      ></style>
+      <Suspense fallback="...">
+        <Editor
+          currentFilepath="/index.tsx"
+          initialFiles={{ "/index.tsx": "console.log('xxx')" }}
+          onChangeFiles={(files) => {
+            console.log(files);
+          }}
+        ></Editor>
+      </Suspense>
+    </>
+  );
+}
+
+const main = document.createElement("main");
+// main.style.width = "80vw";
+// main.style.height = "80vh";
+document.body.append(main);
+
+ReactDOM.render(<App />, main);
